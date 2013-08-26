9df4f70d89c8943d8a29fcf70bb1eb9aabab0597
Nick Chappell <nick@intronic.org>
2013-08-25 22:36:03 -0700

    Added a new view file and MyClass to load it.

---
+++ empty window/MyClass.h
@@ -0,0 +1,13 @@
+//
+//  MyClass.h
+//  empty window
+//
+//  Created by nick on 8/25/13.
+//  Copyright (c) 2013 nick. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MyClass : NSObject
+
+@end
---
+++ empty window/MyClass.m
@@ -0,0 +1,13 @@
+//
+//  MyClass.m
+//  empty window
+//
+//  Created by nick on 8/25/13.
+//  Copyright (c) 2013 nick. All rights reserved.
+//
+
+#import "MyClass.h"
+
+@implementation MyClass
+
+@end
